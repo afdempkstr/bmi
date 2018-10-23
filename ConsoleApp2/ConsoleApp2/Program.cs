5efee33c99d6c43412910f7811c4da9627fd4427
Kostas Stroggylos <constantinos.stroggylos@peoplecert.onmicrosoft.com>
2018-10-23 20:59:54 +0300

    Removed extra space

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             float weight = 80;
-            float height = 1.8f; 
+            float height = 1.8f;
             float bmi = weight/ (height * height);
             // Console.WriteLine prints something to the console
             Console.WriteLine($"BMI is: {bmi}");
