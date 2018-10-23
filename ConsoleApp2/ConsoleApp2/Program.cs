59f7acfa22395ebf3e481c5478ca9a2bf061d88c
Kostas Stroggylos <constantinos.stroggylos@peoplecert.onmicrosoft.com>
2018-10-23 20:36:51 +0300

    Added comments

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -7,8 +7,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             float weight = 80;
-            float height = 1.8f;
+            float height = 1.8f; 
             float bmi = weight/ (height * height);
+            // Console.WriteLine prints something to the console
             Console.WriteLine($"BMI is: {bmi}");
             Console.Read();
         }
