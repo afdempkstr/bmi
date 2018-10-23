ca8152f9dd313f0bf0337878780d70a736fe1df7
Kostas Stroggylos <constantinos.stroggylos@peoplecert.onmicrosoft.com>
2018-10-23 20:30:02 +0300

    First version of BMI calculator

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float weight = 80;
+            float height = 1.8f;
+            float bmi = weight/ (height * height);
+            Console.WriteLine($"BMI is: {bmi}");
+            Console.Read();
+        }
+    }
+}
